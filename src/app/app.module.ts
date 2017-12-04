132e6492dc04d2f7678b1f9bcfae413678543bf2
Qian Yang <qianyang@Qians-MacBook-Pro.local>
2017-12-03 19:08:02 -0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{FormsModule} from '@angular/forms';
+import{HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MyComponentComponent } from './my-component/my-component.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponentComponent
   ],
   imports: [
     BrowserModule
